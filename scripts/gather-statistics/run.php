98b191211143712bbcb614399117cf615e03cb1f
Shane Mc Cormack <shanemcc@gmail.com>
2019-08-06 21:37:09 +0100

    Logging changes.

--- scripts/gather-statistics/run.php
+++ scripts/gather-statistics/run.php
@@ -4,6 +4,22 @@
 	require_once(__DIR__ . '/FakeThread.php');
 	require_once(__DIR__ . '/RedisLock.php');
 
+	$runId = base_convert(crc32(uniqid()), 16, 36);;
+
+	function showTime() {
+		return date('[Y-m-d H:i:s O]');
+	}
+
+	$statsTime = time();
+
+	echo showTime(), ' [', $runId, '] Starting gather-statistics.', "\n";
+
+	if (empty($config['redis']) || !class_exists('Redis')) {
+		die(showTime() . ' [' . $runId . '] Redis is required for GatherStatistics.' . "\n");
+	} else {
+		RedisLock::setRedisHost($config['redis'], $config['redisPort']);
+	}
+
 	if (empty($config['influx']['host']) || empty($config['bind']['slaves'])) { die(0); }
 
 	$client = new InfluxDB\Client($config['influx']['host'], $config['influx']['port']);
@@ -11,35 +27,33 @@
 	$database = $client->selectDB($config['influx']['db']);
 	if (!$database->exists()) { $database->create(); }
 
-	if (empty($config['redis']) || !class_exists('Redis')) {
-		die('Redis is required for GatherStatistics.');
-	} else {
-		RedisLock::setRedisHost($config['redis'], $config['redisPort']);
-	}
 
-	function parseStats($server, $xml, $time = NULL) {
+
+	function parseStats($server, $xml, $statsTime = NULL) {
+		global $runId;
+
 		$xml = simplexml_load_string($xml);
 		$points = [];
 
 		if ($xml === false) {
-			echo 'Failed to parse statistics for server: ', $server, "\n";
+			echo showTime(), ' [', $runId, '] Failed to parse statistics for server: ', $server, "\n";
 			return FALSE;
 		}
 
-		echo 'Parsing statistics for server: ', $server, "\n";
+		echo showTime(), ' [', $runId, '] Parsing statistics for server: ', $server, "\n";
 
-		if ($time == NULL) {
-			$time = strtotime((string)$xml->xpath('/statistics/server/current-time')[0]);
+		if ($statsTime == NULL) {
+			$statsTime = strtotime((string)$xml->xpath('/statistics/server/current-time')[0]);
 		}
 
 		// Global Statistics
 		$query = (int)$xml->xpath('/statistics/server/counters[@type="opcode"]/counter[@name="QUERY"]')[0][0];
-		$points[] = new InfluxDB\Point('opcode_query', $query, ['host' => $server], [], $time);
+		$points[] = new InfluxDB\Point('opcode_query', $query, ['host' => $server], [], $statsTime);
 
 		foreach ($xml->xpath('/statistics/server/counters[@type="qtype"]/counter') as $counter) {
 			$type = (string)$counter['name'];
 			$value = (int)$counter;
-			$points[] = new InfluxDB\Point('qtype', $value, ['host' => $server, 'qtype' => $type], [], $time);
+			$points[] = new InfluxDB\Point('qtype', $value, ['host' => $server, 'qtype' => $type], [], $statsTime);
 		}
 
 		// Per-Zone Statistics
@@ -49,7 +63,7 @@
 			foreach ($zone->xpath('counters[@type="qtype"]/counter') as $counter) {
 				$type = (string)$counter['name'];
 				$value = (int)$counter;
-				$points[] = new InfluxDB\Point('zone_qtype', $value, ['host' => $server, 'qtype' => $type, 'zone' => $zoneName], [], $time);
+				$points[] = new InfluxDB\Point('zone_qtype', $value, ['host' => $server, 'qtype' => $type, 'zone' => $zoneName], [], $statsTime);
 			}
 		}
 
@@ -60,15 +74,11 @@
 		return @file_get_contents('http://' . $host . ':8080/');
 	}
 
-	if (!file_exists(__DIR__ . '/run.lock')) { file_put_contents(__DIR__ . '/run.lock', ''); }
-
-	$time = time();
-
 	if (RedisLock::acquireLock('GatherStatistics', false, 300)) {
-		echo 'Begin statistics.', "\n";
+		echo showTime(), ' [', $runId, '] Begin statistics.', "\n";
 
 		if (FakeThread::available()) {
-			echo 'Threaded.', "\n";
+			echo showTime(), ' [', $runId, '] Threaded.', "\n";
 			$runningThreads = [];
 		}
 
@@ -82,7 +92,7 @@
 			$host = $slave[1];
 
 			if (FakeThread::available()) {
-				echo 'Starting collector Thread for: ', $name, "\n";
+				echo showTime(), ' [', $runId, '] Starting collector Thread for: ', $name, "\n";
 				$thread = new FakeThread('getStats');
 				$thread->start($host);
 				$runningThreads[$name] = ['thread' => $thread, 'type' => 'collector'];
@@ -100,14 +110,14 @@
 				foreach ($currentThreads as $name => $t) {
 					if (!$t['thread']->isAlive()) {
 						unset($runningThreads[$name]);
-						echo $t['type'], ' thread has finished for: ', $name, "\n";
+						echo showTime(), ' [', $runId, '] ', $t['type'], ' thread has finished for: ', $name, "\n";
 
 						if ($t['type'] == 'collector') {
 							$data = $t['thread']->getData();
 
-							echo 'Starting parser Thread for: ', $name, "\n";
+							echo showTime(), ' [', $runId, '] Starting parser Thread for: ', $name, "\n";
 							$thread = new FakeThread('parseStats');
-							$thread->start($name, $data, $time);
+							$thread->start($name, $data, $statsTime);
 							$runningThreads[$name] = ['thread' => $thread, 'type' => 'parser'];
 
 						} else if ($t['type'] == 'parser') {
@@ -119,11 +129,11 @@
 				usleep(100000);
 			}
 
-			echo 'Got all data.', "\n";
+			echo showTime(), ' [', $runId, '] Got all data.', "\n";
 		} else {
 			foreach ($data as $name => $stats) {
 				if (!empty($stats)) {
-					$points[] = parseStats($name, $xml, $time);
+					$points[] = parseStats($name, $xml, $statsTime);
 				}
 			}
 		}
@@ -134,9 +144,9 @@
 				$result = $database->writePoints($p, InfluxDB\Database::PRECISION_SECONDS);
 			}
 		}
-		echo 'End statistics.', "\n";
+		echo showTime(), ' [', $runId, '] End statistics.', "\n";
 
 		RedisLock::releaseLock('GatherStatistics');
 	} else {
-		echo 'Unable to grab statistics, unable to get lock.', "\n";
+		echo showTime(), ' [', $runId, '] Unable to grab statistics, unable to get lock.', "\n";
 	}
